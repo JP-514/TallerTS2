97f4efcd037d1d98d917aa02f0c37de67f3eb78f
Juan Pablo Martinez Pineda <jp.martinezp1@uniandes.edu.co>
2022-11-20 02:16:58 -0500

    Taller Typescript 2

---
+++ scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, channel, seasons, descripcion, web, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.descripcion = descripcion;
+        this.web = web;
+        this.image = image;
+    }
+    return Serie;
+}());
+export { Serie };
